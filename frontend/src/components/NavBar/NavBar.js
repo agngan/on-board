a0e025bad862cb63613f29029c92619a178e02d3
agngan <ganowicz.agnieszka@gmail.com>
2020-07-15 17:53:26 +0200

    Added NavBar prototype.

---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import "./NavBar.css"
+
+class NavBar extends Component {
+    render() {
+        return (
+            <Navbar className="shadow-sm nav-bar navbar-light">
+                <Navbar.Brand justify href="#"><span className="nav-brand-text">OnBoard</span></Navbar.Brand>
+                <Nav className="ml-auto">
+                    <Nav.Item>
+                        <Nav.Link href="#"><span className="nav-item-text">My&nbsp;games</span></Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link>
+                        <Button className="nav-login-button btn-dark"><span className="nav-login-button-text">LOGIN</span></Button>
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar>
+        );
+    }
+}
+
+export default NavBar;
