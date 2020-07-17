af6e06a4a6fdd8da6bc53878af6744e70cac55a0
agngan <ganowicz.agnieszka@gmail.com>
2020-07-17 18:49:51 +0200

    Extracted sliders to a custom component.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,80 +1,28 @@
 import React, {Component} from 'react';
-// import Slider from "react-compound-slider/Slider";
-import { Slider, Rail, Handles, Tracks, Ticks } from "react-compound-slider";
+import CustomSlider from "../components/CustomSlider";
 import "./HomePage.css";
 
-const sliderStyle = {
-    position: 'relative',
-    width: '100%',
-    height: 50
-};
-
-const railStyle = {
-    position: 'absolute',
-    width: '100%',
-    height: 4,
-    marginTop: 35,
-    borderRadius: 5,
-    backgroundColor: '#C4C4C4',
-};
-
-export function Handle({
-                           handle: { id, value, percent },
-                           getHandleProps
-                       }) {
-    return (
-        <div
-            style={{
-                left: `${percent}%`,
-                position: 'absolute',
-                marginLeft: -15,
-                marginTop: 29,
-                zIndex: 2,
-                width: 15,
-                height: 15,
-                border: 0,
-                textAlign: 'center',
-                cursor: 'pointer',
-                borderRadius: '50%',
-                backgroundColor: '#7280FF',
-                color: '#333',
-            }}
-            {...getHandleProps(id)}
-        >
-            <div style={{ fontFamily: 'Roboto', fontSize: 11, marginTop: 22, color: 'white' }}>
-                {value}
-            </div>
-        </div>
-    )
-}
-
-function Track({ source, target, getTrackProps }) {
-    return (
-        <div
-            style={{
-                position: 'absolute',
-                height: 4,
-                zIndex: 1,
-                marginTop: 35,
-                backgroundColor: '#7280FF',
-                borderRadius: 5,
-                cursor: 'pointer',
-                left: `${source.percent}%`,
-                width: `${target.percent - source.percent}%`,
-            }}
-            {...getTrackProps()}
-        />
-    )
-}
+const initialMinNumberOfPlayers = 4;
+const initialPlaytimeRange = [30,90];
 
 class HomePage extends Component {
 
     state = {
-      numberOfPlayers: []
+        minNumberOfPlayers: [initialMinNumberOfPlayers],
+        playtimeRange: initialPlaytimeRange
     };
 
-    onChangeNumberOfPlayers = values => {
-        this.setState({numberOfPlayers: values});
+    onNumberOfPlayersChange = values => {
+        const newState = this.state;
+        newState.minNumberOfPlayers = values;
+        this.setState(newState);
+        console.log(this.state);
+    };
+
+    onPlaytimeRangeChange = values => {
+        const newState = this.state;
+        newState.playtimeRange = values;
+        this.setState(newState);
         console.log(this.state);
     };
 
@@ -83,52 +31,25 @@ class HomePage extends Component {
             <div>
                 <div className="title" >Find a game perfect for you!</div>
                 <div className="slider-left">
-                    <span className="form-title">Number of players:</span>
-                    <Slider
-                        rootStyle={sliderStyle}
-                        domain={[0, 100]}
-                        step={1}
-                        mode={2}
-                        values={[30, 60]}
-                        onChange={this.onChangeNumberOfPlayers}
-                    >
-                        <Rail>
-                            {({ getRailProps }) => (
-                                <div style={railStyle} {...getRailProps()} />
-                            )}
-                        </Rail>
-                        <Handles>
-                            {({ handles, getHandleProps }) => (
-                                <div className="slider-handles">
-                                    {handles.map(handle => (
-                                        <Handle
-                                            key={handle.id}
-                                            handle={handle}
-                                            getHandleProps={getHandleProps}
-                                        />
-                                    ))}
-                                </div>
-                            )}
-                        </Handles>
-                        <Tracks left={false} right={false}>
-                            {({ tracks, getTrackProps }) => (
-                                <div className="slider-tracks">
-                                    {tracks.map(({ id, source, target }) => (
-                                        <Track
-                                            key={id}
-                                            source={source}
-                                            target={target}
-                                            getTrackProps={getTrackProps}
-                                        />
-                                    ))}
-                                </div>
-                            )}
-                        </Tracks>
-                    </Slider>
+                    <span className="form-title">Min. number of players:</span>
+                    <CustomSlider
+                        key={1}
+                        domain={[1,50]}
+                        defaultValues={[initialMinNumberOfPlayers]}
+                        onChange={this.onNumberOfPlayersChange}
+                        left={false}
+                        right={true} />
                 </div>
 
                 <div className="slider-right">
-                    <span className="form-title">Playtime:</span>
+                    <span className="form-title">Playtime in minutes:</span>
+                    <CustomSlider
+                        key={2}
+                        domain={[0, 300]}
+                        defaultValues={initialPlaytimeRange}
+                        onChange={this.onPlaytimeRangeChange}
+                        left={false}
+                        right={false} />
                 </div>
             </div>
         );
