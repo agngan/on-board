a59e2d622b201224627e78be8151cfe14d28f4a4
agngan <ganowicz.agnieszka@gmail.com>
2020-08-25 12:50:36 +0200

    Added table header in Ranking page.

--- frontend/src/pages/Ranking.js
+++ frontend/src/pages/Ranking.js
@@ -1,10 +1,52 @@
 import React, {Component} from 'react';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import './Ranking.css';
 
 class Ranking extends Component {
+
+    state = {
+      gameName: "Bohnanza",
+      scores: [
+          {
+              user: "Agnieszka",
+              score: "150"
+          },
+          {
+              user: "Andrzej",
+              score: "150"
+          },
+          {
+              user: "Ania",
+              score: "150"
+          },
+          {
+              user: "Agata",
+              score: "150"
+          },
+          {
+              user: "Iga",
+              score: "150"
+          },
+          {
+              user: "Szymon",
+              score: "150"
+          }
+      ]
+    };
+
     render() {
         return (
             <div>
-                <span>Ranking</span>
+                <div className="title">Ranking</div>
+
+                <div className="ranking-table-header" >
+                    <Row>
+                        <Col>#</Col>
+                        <Col>Username</Col>
+                        <Col>Score</Col>
+                    </Row>
+                </div>
             </div>
         );
     }
