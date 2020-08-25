c521cbe059fe143f9a1cd0deb5de30cc30fec636
agngan <ganowicz.agnieszka@gmail.com>
2020-08-25 11:33:31 +0200

    Finished prototype of MyGames page.

--- frontend/src/components/GameInfoSmall/GameInfoSmall.js
+++ frontend/src/components/GameInfoSmall/GameInfoSmall.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import "./GameInfoSmall.css";
+import Button from "react-bootstrap/Button";
 
 class GameInfoSmall extends Component {
     render() {
@@ -11,6 +12,24 @@ class GameInfoSmall extends Component {
                         alt={this.props.game.name}
                         style={{height: '100%', width: '100%', objectFit: 'contain'}}/>
                 </div>
+
+                <div className="game-title-small">{this.props.game.name}</div>
+
+                <div className="game-info-small">
+                    <span>My score: {this.props.game.userScore}</span><br/>
+                    <span>Best score overall: {this.props.game.bestScore}</span><br/>
+                    <span>Top player: {this.props.game.topPlayer}</span><br/>
+                    <span>Last win: {this.props.game.lastWin}</span><br/>
+
+                    <div className="game-info-small-buttons">
+                        <Button className="custom-button mt-2"><span
+                            className="custom-button-text">I won!</span></Button>
+                        <Button className="custom-button mt-2"><span
+                            className="custom-button-text">See ranking</span></Button>
+                    </div>
+                </div>
+
+
             </div>
         );
     }
--- frontend/src/pages/MyGames.js
+++ frontend/src/pages/MyGames.js
@@ -3,42 +3,34 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import GameInfoSmall from "../components/GameInfoSmall/GameInfoSmall";
 import "./MyGames.css";
-import GameInfo from "../components/GameInfo/GameInfo";
 
 class MyGames extends Component {
 
     state = {
-        games: [
+        myGames: [
             {
                 name: "Bohnanza",
                 image: "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254186139-51cXhVYYHwL.jpg",
-                players: "2-7",
-                playtime: "45-60",
-                minAge: "13",
-                yearPublished: "1997",
+                userScore: "100",
+                bestScore: "120",
                 topPlayer: "Agnieszka",
-                description: "This great card game is about planting, trading, and selling beans - 11 kinds of beans (this English version includes all the cards from the original game and the first expansion).\n" +
-                    "Players try to collect large sets of beans to sell for gold. There is limited growing space and always new beans to plant. To avoid planting unwanted beans, players trade them to other players who want them for their bean fields."
+                lastWin: "14-08-20"
             },
             {
                 name: "Saboteur",
                 image: "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559253385911-51SYQdt0ZPL.jpg",
-                players: "3-10",
-                playtime: "30-45",
-                minAge: "8",
-                yearPublished: "2004",
+                userScore: "150",
+                bestScore: "170",
                 topPlayer: "Agnieszka",
-                description: "You and your fellow dwarves are digging for gold in a maze of mining tunnels. But, beware! Some of the miners are saboteurs trying to foil your efforts and steal all your hard-earned gold! Now you must overcome cave-ins, broken lanterns, and busted pick-axes to find the mother lode!"
+                lastWin: "23-08-20"
             },
             {
-                name: "Saboteur",
-                image: "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559253385911-51SYQdt0ZPL.jpg",
-                players: "3-10",
-                playtime: "30-45",
-                minAge: "8",
-                yearPublished: "2004",
+                name: "Agricola (Revised Edition)",
+                image: "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254915322-61vm3wX33lL.jpg",
+                userScore: "37",
+                bestScore: "57",
                 topPlayer: "Agnieszka",
-                description: "You and your fellow dwarves are digging for gold in a maze of mining tunnels. But, beware! Some of the miners are saboteurs trying to foil your efforts and steal all your hard-earned gold! Now you must overcome cave-ins, broken lanterns, and busted pick-axes to find the mother lode!"
+                lastWin: "24-08-20"
             }
         ]
     };
@@ -48,7 +40,7 @@ class MyGames extends Component {
             <div>
                 <div className="title">My games:</div>
                 <Row md={2} className="my-games-row">
-                    {this.state.games.map(game => <Col><GameInfoSmall key={game.name} game={game}/></Col>)}
+                    {this.state.myGames.map(game => <Col><GameInfoSmall key={game.name} game={game}/></Col>)}
                 </Row>
             </div>
         );
