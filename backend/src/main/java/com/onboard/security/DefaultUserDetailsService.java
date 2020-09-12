a8488e37b0fd2a3a3532680e2d3f5f599661555b
agngan <ganowicz.agnieszka@gmail.com>
2020-09-12 12:44:24 +0200

    Added authentication on backend.

---
+++ backend/src/main/java/com/onboard/security/DefaultUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.onboard.security;
+
+import com.onboard.entities.User;
+import com.onboard.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public DefaultUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user =  userRepository.findByUsername(username);
+        if (user == null){
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        }
+        return user;
+    }
+}
---
+++ backend/src/main/java/com/onboard/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.onboard.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DefaultUserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(DefaultUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // TODO: add handling different roles
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
