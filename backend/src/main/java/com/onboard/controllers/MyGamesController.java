297bb54e1e46fb6f1bacccd6b054196035d467ef
agngan <ganowicz.agnieszka@gmail.com>
2020-10-02 12:59:09 +0200

    Cleaned up code. Moved getting api key to ClientConfig.

--- backend/src/main/java/com/onboard/controllers/MyGamesController.java
+++ backend/src/main/java/com/onboard/controllers/MyGamesController.java
@@ -1,12 +1,16 @@
 package com.onboard.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onboard.configurations.ClientConfig;
 import com.onboard.entities.Game;
-import com.onboard.entities.User;
 import com.onboard.entities.Win;
 import com.onboard.projections.MyGamesRecord;
 import com.onboard.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +23,7 @@ import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -45,53 +46,36 @@ public class MyGamesController {
         if (!userRepository.existsByUsername(username)) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        User user = userRepository.findByUsername(username);
 
-        for (Game game : user.getGames()) {
-            String imagePath;
-            long myScore;
-            long bestScore = 0L;
-            String topPlayer = "No winners";
-            LocalDate latestWinDate = null;
-
-            try {
-                imagePath = getImagePath(game.getApiId());
-            } catch (IOException e) {
+        for (Game game : userRepository.findByUsername(username).getGames()) {
+            MyGamesRecord myGamesRecord = getMyGamesRecord(game, username);
+            if (myGamesRecord == null)
                 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-            if (imagePath == null)
-                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-
-            Map<String, List<Win>> userWinsMap = game.getWins().stream().collect(Collectors.groupingBy(win -> win.getUser().getUsername()));
-
-//            myScore = game.getWins().stream().filter(win -> win.getUser().getUsername().equals(username)).count();
-            myScore = userWinsMap.get(username).size();
-
-            for (Map.Entry<String, List<Win>> entry : userWinsMap.entrySet()) {
-                int score = entry.getValue().size();
-                if (score > bestScore) {
-                    bestScore = (long) score;
-                    topPlayer = entry.getKey();
-                } else if (score != 0 && score == bestScore) {
-                    Optional<Win> currentPlayerLatestWin = entry.getValue().stream().min((w1, w2) -> w2.getDate().compareTo(w1.getDate()));
-                    Optional<Win> topPlayerLatestWin = userWinsMap.get(topPlayer).stream().min((w1, w2) -> w2.getDate().compareTo(w1.getDate()));
-                    if (currentPlayerLatestWin.get().getDate().compareTo(topPlayerLatestWin.get().getDate()) < 0) {
-                        topPlayer = currentPlayerLatestWin.get().getUser().getUsername();
-                    }
-                }
-            }
-
-            Optional<Win> latestWin = userWinsMap.get(username).stream().min((w1, w2) -> w2.getDate().compareTo(w1.getDate()));
-            if (latestWin.isPresent()) {
-                latestWinDate = latestWin.get().getDate();
-            }
-
-            myGamesRecords.add(new MyGamesRecord(game.getId(), game.getName(), imagePath, myScore, bestScore, topPlayer, latestWinDate));
+            else
+                myGamesRecords.add(myGamesRecord);
         }
         return new ResponseEntity<>(myGamesRecords, HttpStatus.OK);
     }
 
-    private String getImagePath(String ids) throws IOException {
+    private MyGamesRecord getMyGamesRecord(Game game, String username) {
+        String imagePath;
+        long myScore;
+        TopPlayerWithScore topPlayerWithScore = new TopPlayerWithScore("No winners", 0L);
+        LocalDate latestWinDate;
+
+        imagePath = getImagePath(game.getApiId());
+        if (imagePath == null)
+            return null;
+
+        Map<String, List<Win>> userWinsMap = game.getWins().stream().collect(Collectors.groupingBy(win -> win.getUser().getUsername()));
+        myScore = userWinsMap.get(username).size();
+        updateTopPlayerWithScore(topPlayerWithScore, userWinsMap);
+        latestWinDate = getLatestWinDate(username, userWinsMap);
+
+        return new MyGamesRecord(game.getId(), game.getName(), imagePath, myScore, topPlayerWithScore.getBestScore(), topPlayerWithScore.getTopPlayer(), latestWinDate);
+    }
+
+    private String getImagePath(String ids) {
         ObjectMapper objectMapper = new ObjectMapper();
 
         Map<String, String> parameters = getParametersMap(ids, objectMapper);
@@ -102,20 +86,55 @@ public class MyGamesController {
         if (resultJson == null)
             return null;
 
-        Map<String, List<Map<String, Object>>> resultMap = objectMapper.readValue(resultJson, new TypeReference<>() {
-        });
+        Map<String, List<Map<String, Object>>> resultMap;
+        try {
+            resultMap = objectMapper.readValue(resultJson, new TypeReference<>() {
+            });
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+
         return resultMap.get("games").get(0).get("image_url").toString();
     }
 
-    private Map<String, String> getParametersMap(String ids, ObjectMapper objectMapper) throws IOException {
-        // TODO: should it be somewhere else?
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("apiKey.yml");
-        if (inputStream == null) {
+    private Map<String, String> getParametersMap(String ids, ObjectMapper objectMapper) {
+        Map<String, String> parameters = new HashMap<>();
+        try {
+            parameters.put("client_id", new ClientConfig().getApiKey());
+        } catch (IOException e) {
             return null;
         }
-        Map<String, String> parameters = objectMapper.readValue(inputStream, new TypeReference<>() {
-        });
         parameters.put("ids", ids);
         return parameters;
+    }
+
+    private void updateTopPlayerWithScore(TopPlayerWithScore topPlayerWithScore, Map<String, List<Win>> userWinsMap) {
+        for (Map.Entry<String, List<Win>> entry : userWinsMap.entrySet()) {
+            int score = entry.getValue().size();
+            if (score > topPlayerWithScore.getBestScore()) {
+                topPlayerWithScore.setBestScore(score);
+                topPlayerWithScore.setTopPlayer(entry.getKey());
+
+            } else if (score != 0 && score == topPlayerWithScore.getBestScore()) {
+                Optional<Win> currentPlayerLatestWin = entry.getValue().stream().min((w1, w2) -> w2.getDate().compareTo(w1.getDate()));
+                Optional<Win> topPlayerLatestWin = userWinsMap.get(topPlayerWithScore.getTopPlayer()).stream().min((w1, w2) -> w2.getDate().compareTo(w1.getDate()));
+                if (currentPlayerLatestWin.get().getDate().compareTo(topPlayerLatestWin.get().getDate()) < 0) {
+                    topPlayerWithScore.setTopPlayer(currentPlayerLatestWin.get().getUser().getUsername());
+                }
+            }
+        }
+    }
+
+    private LocalDate getLatestWinDate(String username, Map<String, List<Win>> userWinsMap) {
+        Optional<Win> latestWin = userWinsMap.get(username).stream().min((w1, w2) -> w2.getDate().compareTo(w1.getDate()));
+        return latestWin.map(Win::getDate).orElse(null);
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    private class TopPlayerWithScore {
+        String topPlayer;
+        long bestScore;
     }
 }
