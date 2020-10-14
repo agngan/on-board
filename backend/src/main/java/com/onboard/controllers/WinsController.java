b954cafddfc0b4cb350e744b1a9c9eb7be72c1b2
agngan <ganowicz.agnieszka@gmail.com>
2020-10-14 17:24:43 +0200

    Added post method for adding wins.

---
+++ backend/src/main/java/com/onboard/controllers/WinsController.java
@@ -0,0 +1,54 @@
+package com.onboard.controllers;
+
+import com.onboard.entities.Win;
+import com.onboard.pojos.WinInfo;
+import com.onboard.pojos.WinValidationData;
+import com.onboard.repositories.GameRepository;
+import com.onboard.repositories.UserRepository;
+import com.onboard.repositories.WinRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("${spring.data.rest.base-path}")
+public class WinsController {
+
+    private final UserRepository userRepository;
+    private final WinRepository winRepository;
+    private final GameRepository gameRepository;
+
+    public WinsController(UserRepository userRepository, WinRepository winRepository, GameRepository gameRepository) {
+        this.userRepository = userRepository;
+        this.winRepository = winRepository;
+        this.gameRepository = gameRepository;
+    }
+
+    @PostMapping("addWin/{username}")
+    public ResponseEntity<List<String>> addWin(@PathVariable String username, @RequestBody WinInfo winInfo){
+        List<String> errors = new ArrayList<>();
+        for (WinValidationData validation : winInfo.getValidations()){
+            if (validation.getUsername().equals(username)){
+                errors.add("You cannot use yourself to validate you win");
+            }
+            if (!userRepository.existsByUsername(validation.getUsername())){
+                errors.add("User with username '" + validation.getUsername() + "' does not exist");
+            } else if (!userRepository.findByUsername(validation.getUsername()).getSecretCode().equals(validation.getSecretCode())) {
+                errors.add("Wrong secret code for user '" + validation.getUsername() + "'");
+            }
+        }
+        if (!errors.isEmpty()){
+            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        }
+
+        if (!gameRepository.existsById(winInfo.getGameId())){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        winRepository.save(new Win(LocalDate.now(), userRepository.findByUsername(username), gameRepository.getById(winInfo.getGameId())));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
